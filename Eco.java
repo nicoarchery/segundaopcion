a2b028b3f663aebd3c83ce24aa1fcfaf640569cd
Nicolas Jimenez Vargas <1006107324@u.icesi.edu.co>
2023-08-16 12:19:17 -0500

    Agregando el archivo de la calculadora

---
+++ Eco.java
@@ -0,0 +1,38 @@
+
+
+
+import java.util.Scanner;
+
+public class Eco { //Encabezado de clase
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola, Ingresa dos numeros para ser operados entre ellos");
+
+		Scanner entrada1 = new Scanner(System.in);
+
+		Scanner entrada2 = new Scanner(System.in);
+
+		int numero1 = entrada1
+
+		int numero2 = entrada2
+
+		int suma = entrada1 + entrada2
+
+		int resta = entrada1 - entrada2
+
+		int multiplicacion = entrada1 * entrada2
+
+		int division = entrada1 / entrada2
+
+		int modulo = entrada1 % entrada2
+
+		String mensaje = entrada.nextLine();
+
+		System.out.println("el resultado de la suma es: " + suma);
+		System.out.println("el resultado de la resta es: " + resta);
+		System.out.println("el resultado de la multiplicacion es: " + multiplicacion);
+		System.out.println("el resultado de la division es: " + division);
+		System.out.println("el resultado de la suma es: " + modulo);
+	}
+}
